81ae459982d06941d86cfd89480d2635f7b93b5e
Aldo <aldoacesso@gmail.com>
2024-08-29 11:29:19 -0300

    feat(filter): created and styled the filter.component

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,16 +1,23 @@
 import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+
 import { AngularMaterialModule } from "../angular-material/angular-material.module";
+
 import { UserDetailsComponent } from "./user-details/user-details.component";
+import { FilterComponent } from './filter/filter.component';
 
 @NgModule({
   declarations: [
-    UserDetailsComponent
+    UserDetailsComponent,
+    FilterComponent
   ],
   imports: [
+    FormsModule,
     AngularMaterialModule
   ],
   exports: [
-    UserDetailsComponent
+    UserDetailsComponent,
+    FilterComponent
   ]
 })
 export class ComponentsModule {}
