2c596c22b9d336561bfbcc271153577f369799d7
Aldo <aldoacesso@gmail.com>
2024-08-30 16:08:21 -0300

    feat(components.module): added pipes.module

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -6,6 +6,7 @@ import { AngularMaterialModule } from "../angular-material/angular-material.modu
 import { UserDetailsComponent } from "./user-details/user-details.component";
 import { FilterComponent } from './filter/filter.component';
 import { UsersListComponent } from './users-list/users-list.component';
+import { PipesModule } from "../pipes/pipes.module";
 
 @NgModule({
   declarations: [
@@ -15,7 +16,8 @@ import { UsersListComponent } from './users-list/users-list.component';
   ],
   imports: [
     FormsModule,
-    AngularMaterialModule
+    AngularMaterialModule,
+    PipesModule
   ],
   exports: [
     UserDetailsComponent,
