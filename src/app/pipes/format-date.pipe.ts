a67d3dfa056e5978684bb972e94942b2abde0fb3
Aldo <aldoacesso@gmail.com>
2024-08-30 16:21:38 -0300

    feat(pipes): created pipes and added to pipes.module

---
+++ src/app/pipes/format-date.pipe.ts
@@ -0,0 +1,17 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'formatDate'
+})
+export class FormatDatePipe implements PipeTransform {
+
+  transform(formatDate: string): string {
+
+      if (formatDate) {
+      let date = new Date(formatDate);
+      return `${date.getDate().toString().padStart(2, '0')}/${(date.getMonth() + 1).toString().padStart(2, '0')}/${date.getFullYear()}`;
+    }
+    return '';
+  }
+
+}
